24bf9790ce0b8af10f2278f699b7d4e396b73934
StaabJoar <staab.joar@web.de>
2017-09-06 12:16:05 +0200

    soruce Diretory

---
+++ src/Uebungen/Uebung3/ray.js
@@ -0,0 +1,23 @@
+class Ray {
+  /**
+   * Creates a new ray with origin and direction
+   * @param  {Vector} origin    The origin of the Ray
+   * @param  {Vector} direction The direction of the Ray
+   */
+  constructor(origin, direction) {
+    // TODO
+  }
+
+  /**
+   * Creates a ray from the camera through the image plane. The plane is located at (0, 0, -1) with x ∈ [-1,1] and y ∈ [-1, 1]
+   * @param  {number} width  Width of the canvas
+   * @param  {number} height Height of the canvas
+   * @param  {number} xpos   x-position of pixel in canvas
+   * @param  {number} ypos   y-position of pixel in canvas
+   * @param  {Vector} camera Camera-position
+   * @return {Ray}        The resulting Ray
+   */
+  static makeRay(width, height, xpos, ypos, camera) {
+    // TODO
+  }
+}
---
+++ src/Uebungen/Uebung3/sphere.js
@@ -0,0 +1,19 @@
+class Sphere {
+  /**
+   * Creates a new Sphere with center and radius
+   * @param  {Vector} center The center of the Sphere
+   * @param  {number} radius The radius of the Sphere
+   */
+  constructor(center, radius) {
+    // TODO
+  }
+
+  /**
+   * Calculates the intersection of the sphere with the given ray
+   * @param  {Ray} ray The ray to intersect with
+   * @return {number}     The t for the intersection closest to the ray origin. Null if there is no intersection.
+   */
+  intersect(ray) {
+    // TODO
+  }
+}
