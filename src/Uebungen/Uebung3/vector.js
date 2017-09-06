78d2bea0efd1f6b484afa6f92c0874a7762ba780
StaabJoar <staab.joar@web.de>
2017-09-06 11:57:06 +0200

    soruce Diretory

---
+++ src/Uebungen/Uebung3/vector.js
@@ -0,0 +1,126 @@
+class Vector {
+  /**
+   * Create a vector
+   * @param  {number} x The x component
+   * @param  {number} y The y component
+   * @param  {number} z The z component
+   * @param  {number} w The w component
+   * @return {number}   The resulting vector
+   */
+  constructor(x, y, z, w) {
+    // TODO
+  }
+
+  /**
+   * Returns the x component of the vector
+   * @return {number} The x component of the vector
+   */
+  get x() {
+    // TODO
+  }
+
+  /**
+   * Returns the y component of the vector
+   * @return {number} The y component of the vector
+   */
+  get y() {
+    // TODO
+  }
+
+  /**
+   * Returns the z component of the vector
+   * @return {number} The z component of the vector
+   */
+  get z() {
+    // TODO
+  }
+
+  /**
+   * Returns the w component of the vector
+   * @return {number} The w component of the vector
+   */
+  get w() {
+    // TODO
+  }
+
+  /**
+   * Creates a new vector with the scalar/vector added
+   * @param {(number|Vector)} other The scalar/vector to add
+   */
+  add(other) {
+    // TODO
+  }
+
+  /**
+   * Creates a new vector with the scalar/vector subtracted
+   * @param {(number|Vector)} other The scalar/vector to subtract
+   */
+  sub(other) {
+    // TODO
+  }
+
+  /**
+   * Creates a new vector with the scalar/vector piecewise multiplied
+   * @param {(number|Vector)} other The scalar/vector to multiply
+   */
+  mul(other) {
+    // TODO
+  }
+
+  /**
+   * Creates a new vector with the scalar/vector piecewise divided
+   * @param {(number|Vector)} other The scalar/vector to divide
+   */
+  div(other) {
+    // TODO
+  }
+
+  /**
+   * Dot product
+   * @param {Vector} other The vector to calculate the dot product with
+   */
+  dot(other) {
+    // TODO
+  }
+
+  /**
+   * Cross product
+   * @param {Vector} other The vector to calculate the cross product with
+   */
+  cross(other) {
+    // TODO
+  }
+
+  /**
+   * Returns an array representation of the vector
+   * @return {Array} An array representation.
+   */
+  valueOf() {
+    // TODO
+  }
+
+  /**
+   * Creates a new vector by normalising the vector
+   * @return {Vector} A vector with length 1
+   */
+  normalised() {
+    // TODO
+  }
+
+  /**
+   * Compares the vector to another
+   * @param  {Vector} other The vector to compare to.
+   * @return {Boolean}       True if the vectors carry equal numbers. The fourth element may be both equivalent to undefined to still return true.
+   */
+  equals(other) {
+    // TODO
+  }
+
+  /**
+   * Calculates the length of the vector
+   * @return {number} Length of the vector
+   */
+  get length() {
+    // TODO
+  }
+}
